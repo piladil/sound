0f3c1e3e4cdda938a525bf530d71be2a3c483efd
dil <dil@crossovernepal.com>
2015-01-14 07:03:28 -0500

    array

--- web/index.php
+++ web/index.php
@@ -1,6 +1,7 @@
 the php started here
 the phh is pre processor hypertext php
-<?php
-echo "<br>this is preprocessro hypertext php"
 
+<?php
+$cars = array("Volvo", "BMW", "Toyota");
+echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
 ?>
