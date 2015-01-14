a35012b2ef03b8934363b6cba953c225606c4c88
dil <dil@crossovernepal.com>
2015-01-14 06:30:12 -0500

    favourite colour

--- web/index.php
+++ web/index.php
@@ -1,4 +1,18 @@
 the php started here
 <?php
-phpinfo();
+$favcolor = "red";
 
+switch ($favcolor) {
+     case "red":
+         echo "Your favorite color is red!";
+         break;
+     case "blue":
+         echo "Your favorite color is blue!";
+         break;
+     case "green":
+         echo "Your favorite color is green!";
+         break;
+     default:
+         echo "Your favorite color is neither red, blue, or green!";
+}
+?>
